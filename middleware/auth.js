b8f756b27d7d7d8824e0b684af75512e86660e38
Carlos Sosa <carlossosa54321@gmail.com>
2022-09-15 16:54:58 -0700

    changed some code for better understanding

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,18 @@
-module.exports = {
-    ensureAuth: function (req, res, next) {
-      if (req.isAuthenticated()) {
-        return next()
-      } else {
-        res.redirect('/')
-      }
-    }
+module.exports = function ensureAuth(req,res,next){
+  if(req.isAuthenticated()){
+    return next()
+  }else{
+    res.redirect('/')
   }
+}
+
+// module.exports = {
+//     ensureAuth: function (req, res, next) {
+//       if (req.isAuthenticated()) {
+//         return next()
+//       } else {
+//         res.redirect('/')
+//       }
+//     }
+//   }
   
--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const todosController = require('../controllers/todos') 
-const { ensureAuth } = require('../middleware/auth')
+//const { ensureAuth } = require('../middleware/auth')
+const autho = require('../middleware/auth')
 
-router.get('/', ensureAuth, todosController.getTodos)
+router.get('/', autho, todosController.getTodos)
 
 router.post('/createTodo', todosController.createTodo)
 
